1d242540dc87d9393a3c3e20057f766400fd244f
Darius Brown <dariustb@tech-center.com>
2023-09-03 11:24:48 -0400

    Added color scheme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cs-white':  '#fffcf2',
+        'cs-beige':  '#ccc5b9',
+        'cs-brown':  '#403d39',
+        'cs-black':  '#252422',
+        'cs-orange': '#eb5e28',
+      }
+    },
   },
   plugins: [],
 }
-
