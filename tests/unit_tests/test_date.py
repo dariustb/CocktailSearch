3b33e9c1e692dcd7e2537d90e90a3cf77b86a97f
Darius Brown <dariustb@tech-center.com>
2023-09-07 22:05:47 -0400

    Removed unused date files

--- tests/unit_tests/test_date.py
+++
@@ -1,9 +0,0 @@
-''' test_date.py - unit tests for date.py '''
-
-# pylint: disable=E0401, W0621
-
-from src.py import date
-
-def test_get_date_is_string():
-    ''' tests get_date function '''
-    assert isinstance(date.get_date(), str)
