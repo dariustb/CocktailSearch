2ad8bac6b7a7da2f59c2f6d7691856e35972de71
Darius Brown <dariustb@tech-center.com>
2023-09-07 21:58:01 -0400

    Pylint fixes

--- src/py/search_engine.py
+++ src/py/search_engine.py
@@ -14,17 +14,17 @@ class SearchEngine:
         ''' Returns user's query search '''
         query = input('Enter cocktail to search: ')
         return query.title()
-    
+
     def get_all_cocktails(self) -> list:
         ''' Returns all drinks in database '''
-        # command = f'SELECT * from cocktails ORDER BY base, name'
-        command = f'SELECT * FROM cocktails WHERE id >= 1 AND id <=10'
+        # command = 'SELECT * from cocktails ORDER BY base, name'
+        command = 'SELECT * FROM cocktails WHERE id >= 1 AND id <=10'
         return self.d_db.sql_command(command)
-    
+
     def get_all_bases(self) -> list:
         ''' Returns all bases in database '''
-        # command = f'SELECT DISTINCT base from cocktails ORDER BY base'
-        command = f'SELECT DISTINCT base from cocktails WHERE id >= 1 AND id <=10 ORDER BY base'
+        # command = 'SELECT DISTINCT base from cocktails ORDER BY base'
+        command = 'SELECT DISTINCT base from cocktails WHERE id >= 1 AND id <=10 ORDER BY base'
         return self.d_db.sql_command(command)
 
     def search_cocktail(self, cocktail_name: str) -> list:
