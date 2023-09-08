323e56e630cfc829415c95b37f9eddeb8df3be5f
Darius Brown <dariustb@tech-center.com>
2023-09-07 21:15:12 -0400

    Added open() for page renders

--- src/py/database.py
+++ src/py/database.py
@@ -8,15 +8,19 @@ class Database:
     ''' Sqlite3 Database class '''
     def __init__(self):
         ''' Database attributes '''
-        self.d_connection = sqlite3.connect(const.SQL_DB_PATH)
-        self.d_cursor     = self.d_connection.cursor()
+        self.d_connection = None
+        self.d_cursor     = None
         print('Connected to the database')
 
-    def close_connection(self) -> None:
+    def open(self) -> None:
+        ''' Start sql connection safely '''
+        self.d_connection             = sqlite3.connect(const.SQL_DB_PATH)
+        self.d_cursor                 = self.d_connection.cursor()
+
+    def close(self) -> None:
         ''' End sql connection safely '''
         self.d_connection.close()
 
     def sql_command(self, command: str) -> list:
         ''' Run a sql command & returns fetched data '''
-        self.d_cursor.execute(command)
-        return self.d_cursor.fetchall()
+        return self.d_cursor.execute(command).fetchall()
