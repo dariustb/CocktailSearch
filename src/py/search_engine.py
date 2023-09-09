0c2dbb0e7bf4f84d5227dfe4c8eeb6e4b40ff226
Darius Brown <dariustb@tech-center.com>
2023-09-08 23:18:47 -0400

    Added search functionality

--- src/py/search_engine.py
+++ src/py/search_engine.py
@@ -10,12 +10,7 @@ class SearchEngine:
         ''' SearchEngine attributes '''
         self.d_db = db
 
-    def get_search_query(self) -> str:
-        ''' Returns user's query search '''
-        query = input('Enter cocktail to search: ')
-        return query.title()
-
-    def get_all_cocktails(self) -> list:
+    def get_all_elements(self) -> list:
         ''' Returns all drinks in database '''
         # command = 'SELECT * from cocktails ORDER BY base, name'
         command = 'SELECT * FROM cocktails WHERE id >= 1 AND id <=10'
@@ -27,7 +22,17 @@ class SearchEngine:
         command = 'SELECT DISTINCT base from cocktails WHERE id >= 1 AND id <=10 ORDER BY base'
         return self.d_db.sql_command(command)
 
-    def search_cocktail(self, cocktail_name: str):
-        ''' Makes a search through a given database file '''
-        command = f'SELECT * FROM cocktails WHERE name LIKE \'%{cocktail_name}%\''
+    def search_exact(self, query: str):
+        ''' Makes an exact search in database'''
+        command = f'SELECT * FROM cocktails WHERE name = \'{query.title()}\''
         return self.d_db.sql_command(command)[0]
+    
+    def search_similar(self, query: str) -> list:
+        ''' Makes a LIKE search in database'''
+        command = f'SELECT * FROM cocktails WHERE name LIKE \'%{query}%\' ORDER BY base'
+        return self.d_db.sql_command(command)
+
+    def search_similar_bases(self, query: str) -> list:
+        ''' Makes a similar search & returns the bases of those results '''
+        command = f'SELECT DISTINCT base from cocktails WHERE name LIKE \'%{query}%\' ORDER BY base'
+        return self.d_db.sql_command(command)
